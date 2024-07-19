e2d24339bfe2b84fcd807206d88768196b885367
georgeisonearth <89208162+GeorgeIsA@users.noreply.github.com>
2024-07-19 13:52:19 +0300

    Bug fixes + reset button functionality

--- Assets/Scripts/PlayerNames.cs
+++ Assets/Scripts/PlayerNames.cs
@@ -2,9 +2,9 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-
 public class PlayerNames : MonoBehaviour
 {
+    private string nullString;
     public void NameSet()
     {
         if (gameObject.CompareTag("Player1Input"))
@@ -16,21 +16,16 @@ public class PlayerNames : MonoBehaviour
     {
         if (SceneManager.GetActiveScene().name == "GameScene")
         {
-
             if (gameObject.CompareTag("Player1Text"))
             {
-                if (string.IsNullOrWhiteSpace(gameObject.GetComponent<TextMeshProUGUI>().text))
+                if (string.IsNullOrEmpty(PlayerPrefs.GetString("Player1Name")))
                     gameObject.GetComponent<TextMeshProUGUI>().text = "Player 1";
                 else gameObject.GetComponent<TextMeshProUGUI>().text = PlayerPrefs.GetString("Player1Name", "Player 1");
             }
             else if (gameObject.CompareTag("Player2Text"))
             {
-                if (string.IsNullOrWhiteSpace(gameObject.GetComponent<TextMeshProUGUI>().text))
-                {
-                    Debug.Log("Player 2");
-
+                if (string.IsNullOrEmpty(PlayerPrefs.GetString("Player2Name")))
                     gameObject.GetComponent<TextMeshProUGUI>().text = "Player 2";
-                }
                 else gameObject.GetComponent<TextMeshProUGUI>().text = PlayerPrefs.GetString("Player2Name", "Player 2");
             }
         }
@@ -42,5 +37,12 @@ public class PlayerNames : MonoBehaviour
                 gameObject.GetComponent<TMP_InputField>().text = PlayerPrefs.GetString("Player2Name", "Player 1");
         }
 
+    }
+    public void ResetClick()
+    {
+        PlayerPrefs.SetString("Player1Input", null);
+        PlayerPrefs.SetString("Player2Input", null);
+        GameObject.FindGameObjectWithTag("Player1Input").GetComponent<TMP_InputField>().text = null;
+        GameObject.FindGameObjectWithTag("Player2Input").GetComponent<TMP_InputField>().text = null;
     }
 }
